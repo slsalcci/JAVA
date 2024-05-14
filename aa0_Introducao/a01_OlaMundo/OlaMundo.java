a7c65b9462c0452ece6257189e9f00a94559062d
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-13 23:40:14 -0300

    Create OlaMundo.java

---
+++ aa0_Introducao/a01_OlaMundo/OlaMundo.java
@@ -0,0 +1,33 @@
+public class OlaMundo {
+
+	public static void main(String[] args) {	
+	System.out.println("Olá Mundo!");
+	}
+}
+
+/*
+Observação(1): 	em Java o nome da classe deve ser igual ao nome do arquivo: 
+		            OlaMundo.java --> public class OlaMundo {...}
+  
+Observação(2): 	o nome da classe deve sempre iniciar com a primeira letra maiúscula
+
+Observação(3): 	Java segue o padrão CamelCase
+
+Observação(4):
+a) public class: Declaração da Classe
+b) OlaMundo: Nome da Classe
+
+c) Método main():
+                  public static void main(String[] args) {
+                  		System.out.println("Olá Mundo!");
+                  	}
+
+d) public static: Modificadores de Acesso do Método.
+e) void: Tipo de Retorno do Método. Significa que não tem retorno (ou retorno vazio). 
+f) main(): Nome do Método.
+g) (String[] args): Parâmetros/Argumentos do Método (Array de String).
+h) System. out. println(); : é o nome de uma função que serve para escrever informações na tela.
+l) ("Olá Mundo!"); : String deve ser escrita entre aspas duplas.
+j) {}: Blocos estão entre {} (chaves)
+
+*/
