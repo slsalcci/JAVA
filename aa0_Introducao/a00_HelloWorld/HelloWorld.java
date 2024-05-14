ab4e11e126e7af2f61b13f92c625bbcb2a89eb11
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-13 23:30:41 -0300

    Create HelloWorld.java
    Canal no YouTube: Spencer Lewis Salcci
    https://www.youtube.com/watch?v=21OGm0HE29s

---
+++ aa0_Introducao/a00_HelloWorld/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World!");
+	}
+}
+
+/*
+Canal no YouTube: Spencer Lewis Salcci
+https://www.youtube.com/watch?v=21OGm0HE29s
+
+Observação(1): 	em Java o nome da classe deve ser igual ao nome do arquivo: 
+		            HelloWorld.java --> public class HelloWorld {...}
+  
+Observação(2): 	o nome da classe deve sempre iniciar com a primeira letra maiúscula
+
+Observação(3): 	Java segue o padrão CamelCase
+
+
+Observaça]ão(4):
+a) public class: Declaração da Classe
+b) HelloWorld: Nome da Classe
+
+c) Método main():
+                public static void main(String[] args) {
+                		System.out.println("Hello World!");
+                	}
+
+d) public static: Modificadores de Acesso do Método.
+e) void: Tipo de Retorno do Método. Significa que não tem retorno (ou retorno vazio). 
+f) main(): Nome do Método.
+g) (String[] args): Parâmetros/Argumentos do Método (Array de String).
+h) System. out. println(); : é o nome de uma função que serve para escrever informações na tela.
+l) ("Hello World!"); : String deve ser escrita entre aspas duplas.
+j) {}: Blocos estão entre {} (chaves)
+
+*/
