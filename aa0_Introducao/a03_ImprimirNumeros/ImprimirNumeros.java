1436a55885058511fadd83fcf74b228fb53c8b6d
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-21 20:27:46 -0300

    Create ImprimirNumeros.java

---
+++ aa0_Introducao/a03_ImprimirNumeros/ImprimirNumeros.java
@@ -0,0 +1,18 @@
+public class ImprimirNumeros {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Você também pode usar o println() método para imprimir números.");
+		System.out.println("No entanto, ao contrário do texto, não colocamos números entre aspas duplas.");
+		System.out.println(9);
+		System.out.println(89);
+		System.out.println(789);
+		System.out.println(6789);
+		System.out.println(56789);
+		System.out.println(456789);
+		System.out.println(3456789);
+		System.out.println(23456789);
+		System.out.println(123456789);
+		System.out.println(0000000000);
+	}
+}
