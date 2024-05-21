cf65c99ce4afdac94017296eabfa66a0edde35b2
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-21 20:32:32 -0300

    Create ComentariosJava.java

---
+++ aa0_Introducao/a05_ComentariosJava/ComentariosJava.java
@@ -0,0 +1,22 @@
+public class ComentariosJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Comentários Java");   // Este é um comentário de linha única
+		System.out.println("Comentários podem ser usados para explicar o código Java e torná-lo mais legível. Também pode ser usado para impedir a execução ao testar código alternativo.");
+
+		System.out.println("Comentários de linha única");   // Este é um comentário de linha única
+		System.out.println("Comentários de linha única começam com duas barras ( // ). Qualquer texto entre ( // ) e o final da linha é ignorado pelo Java (não será executado).");
+
+		System.out.println("Comentários multilinhas Java");
+		System.out.println("Comentários de várias linhas começam ( /* ) e terminam com ( */ ). Qualquer texto entre ( /* ) e ( */ ) será ignorado pelo Java.");
+
+
+/*
+	Comentários multilinhas Java.
+	Comentários de várias linhas começam ( / * ) e terminam com ( * / ). 
+	Qualquer texto entre ( / * ) e ( * / ) será ignorado pelo Java.
+*/
+
+	}
+}
