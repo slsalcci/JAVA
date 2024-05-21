965a9d67a01e890bbf13433cbf723ec87c597f83
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-21 20:33:38 -0300

    Create ImprimirCalculoMatematico.java

---
+++ aa0_Introducao/a04_ImprimirCalculoMatematico/ImprimirCalculoMatematico.java
@@ -0,0 +1,11 @@
+public class ImprimirCalculoMatematico {
+
+	public static void main(String[] args) {
+
+		System.out.println("Você também pode realizar cálculos matemáticos dentro do println() método.");
+		System.out.println(9 + 7);
+		System.out.println(6 - 4);
+		System.out.println(3 * 5);
+		System.out.println(8 / 2);
+	}
+}
