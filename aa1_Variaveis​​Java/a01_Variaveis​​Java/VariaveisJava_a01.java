5b0e6647d960e226e7dc7c661e7fccae8fb223c3
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 16:19:44 -0300

    Update VariaveisJava_a01.java

--- aa1_Variaveis​​Java/a01_Variaveis​​Java/VariaveisJava_a01.java
+++ aa1_Variaveis​​Java/a01_Variaveis​​Java/VariaveisJava_a01.java
@@ -2,7 +2,7 @@ public class VariaveisJava_a01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Variáveis Java");
+		System.out.println("Variáveis Java - a01");
 		System.out.println("-------------------------------------------------------------");
 		System.out.println("Exemplo: Crie uma variável chamada nome do tipo String e atribua a ela o valor \"Augusta\": \n");
 		System.out.println("String nome = \"Augusta\";");
