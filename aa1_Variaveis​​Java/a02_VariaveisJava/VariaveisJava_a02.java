6d6e60e081ba22cacf668d129ab3968584613ebc
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 16:19:20 -0300

    Update and rename VariaveisJava.java to VariaveisJava_a02.java

--- aa1_Variaveis​​Java/a02_VariaveisJava/VariaveisJava_a02.java
+++ aa1_Variaveis​​Java/a02_VariaveisJava/VariaveisJava_a02.java
@@ -1,8 +1,8 @@
-public class VariaveisJava {
+public class VariaveisJava_a02 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Variáveis Java");
+		System.out.println("Variáveis Java - a02");
 		System.out.println("-------------------------------------------------------------");
 		System.out.println("Exemplo: Crie uma variável que armaze um numero, chamada numero do tipo int  e atribua a ela o valor 49: \n");
 		System.out.println("int numero = 49;");
