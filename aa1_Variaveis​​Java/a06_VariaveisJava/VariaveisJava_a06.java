e6b20cc9d910cb144c650e9e07c27e1663138f0d
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 17:15:05 -0300

    Create VariaveisJava_a06.java

---
+++ aa1_Variaveis​​Java/a06_VariaveisJava/VariaveisJava_a06.java
@@ -0,0 +1,14 @@
+/*
+Crie uma variável chamada carroNome e atribua a ela o valor "Fusca"
+*/
+
+
+public class VariaveisJava_a06 {
+
+	public static void main(String[] args) {
+	
+	String carroNome = "Fusca";
+	System.out.println(carroNome);
+
+	}
+}
