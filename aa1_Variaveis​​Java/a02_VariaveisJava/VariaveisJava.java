b17b8f154cbdb98c8fced7526772e5e150508329
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-21 21:23:11 -0300

    Create VariaveisJava.java

---
+++ aa1_Variaveis​​Java/a02_VariaveisJava/VariaveisJava.java
@@ -0,0 +1,15 @@
+public class VariaveisJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Variáveis Java");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("Exemplo: Crie uma variável que armaze um numero, chamada numero do tipo int  e atribua a ela o valor 49: \n");
+		System.out.println("int numero = 49;");
+		System.out.println("System.out.println(numero);\n");
+		System.out.println("-------------------------------------------------------------");
+
+		int numero = 49;
+		System.out.println(numero);
+	}
+}
