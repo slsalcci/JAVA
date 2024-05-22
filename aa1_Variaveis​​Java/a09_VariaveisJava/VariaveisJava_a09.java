70220baa2fd525bbb7760c164f621c2c521cecfb
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 17:45:09 -0300

    Create VariaveisJava_a09.java

---
+++ aa1_Variaveis​​Java/a09_VariaveisJava/VariaveisJava_a09.java
@@ -0,0 +1,26 @@
+/*
+Crie uma variável chamada t, atribua x + y a ela e exiba o resultado.
+*/
+
+public class VariaveisJava_a09 {
+
+	public static void main(String[] args) {
+	
+	int x = 17;
+	int y = 20;
+	int t = x + y;
+	System.out.println(t);
+	
+	}
+}
+
+/*
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava>cd a09_VariaveisJava
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a09_VariaveisJava>javac VariaveisJava_a09.java
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a09_VariaveisJava>java VariaveisJava_a09
+37
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a09_VariaveisJava>
+*/
