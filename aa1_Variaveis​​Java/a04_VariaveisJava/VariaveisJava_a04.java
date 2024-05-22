ad3fefcb1360df83fa1c69602da6feb4bfba1793
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 16:37:32 -0300

    Create VariaveisJava_a04.java

---
+++ aa1_Variaveis​​Java/a04_VariaveisJava/VariaveisJava_a04.java
@@ -0,0 +1,9 @@
+public class VariaveisJava_a04 {
+
+	public static void main(String[] args) { 
+	
+		int numero = 53;
+		numero = 17; // alterando numero de 53 para 17
+		System.out.println(numero);
+	}
+}
