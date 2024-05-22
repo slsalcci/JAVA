6f81005e4c1d3a2c54c2ae6a34b8777aad54a027
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 17:36:10 -0300

    Create VariaveisJava_a08.java

---
+++ aa1_Variaveis​​Java/a08_VariaveisJava/VariaveisJava_a08.java
@@ -0,0 +1,25 @@
+/*
+Exiba a soma de 51 + 99, usando duas variáveis: x e y.
+*/
+
+public class VariaveisJava_a08 {
+
+	public static void main(String[] args) {
+	
+		int x = 51;
+		int y = 99;
+		System.out.println(x + y);
+	}
+}
+
+
+/*
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava>cd a08_VariaveisJava
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a08_VariaveisJava>javac VariaveisJava_a08.java
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a08_VariaveisJava>java VariaveisJava_a08
+150
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a08_VariaveisJava>
+*/
