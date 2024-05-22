800c0dc86e63369e515cf86269847d256d58007a
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 17:25:05 -0300

    Create VariaveisJava_a07.java

---
+++ aa1_Variaveis​​Java/a07_VariaveisJava/VariaveisJava_a07.java
@@ -0,0 +1,24 @@
+/*
+Crie uma variável chamada velocidadeMaxima e atribua o valor 120 a ela.
+*/
+
+
+public class VariaveisJava_a07 {
+
+	public static void main(String[] args) {
+
+	int velocidadeMaxima = 120;
+	System.out.println(velocidadeMaxima);
+	}
+}
+
+
+
+/*
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a07_VariaveisJava>javac VariaveisJava_a07.java
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a07_VariaveisJava>java VariaveisJava_a07
+120
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a07_VariaveisJava>
+*/
