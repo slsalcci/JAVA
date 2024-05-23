866ca410dcaaae6bf8cecb62efde0073266b55bd
Spencer Lewis Salcci <56243554+slsalcci@users.noreply.github.com>
2024-05-22 21:15:42 -0300

    Create VariaveisJava_a10.java

---
+++ aa1_Variaveis​​Java/a10_VariaveisJava/VariaveisJava_a10.java
@@ -0,0 +1,22 @@
+/*
+Três variáveis ​​do mesmo tipo (int), usando uma lista separada por vírgulas.
+*/
+
+public class VariaveisJava_a10 {
+
+	public static void main(String[] args) {
+		int x = 10, y = 21, t = 32;
+		System.out.println(x + y + t); 
+
+	}
+}
+
+
+/*
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a10_VariaveisJava>javac VariaveisJava_a10.java
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a10_VariaveisJava>java VariaveisJava_a10
+63
+
+C:\Users\Pichau\Documents\Java-Basico\a2_VariaveisJava\a10_VariaveisJava>
+*/
